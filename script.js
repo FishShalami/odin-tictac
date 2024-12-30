0dca5c8721fb6f0596bc36d6bdba53a0effafef6
FishShalami <fishshalami@gmail.com>
2024-12-30 12:43:41 -0800

    New functions for entering value, playing round, and play game. Basic logic complete.

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+
+
+
+
 const board = (function() {
     const board = [];
     const rows = 3;
@@ -12,8 +16,11 @@ const board = (function() {
         return board
 })();
 
+function displayBoard() {
+    console.table(board.map(row => row.map(cell => cell.val)));
+}
 
-//create a player function that creates a player
+
 
 function createPlayer(name, XorO) {
     return {
@@ -33,19 +40,7 @@ function createPlayer(name, XorO) {
     }; 
 } 
 
-const player1 = createPlayer('Dave', 'X');
-const player2 = createPlayer('Bob', 'O');
-board[0][0].val = '3'
-board[0][1].val = '2'
-board[0][2].val = '3'
-board[1][0].val = '4'
-board[1][1].val = '3'
-board[1][2].val = '3'
-board[2][0].val = '8'
-board[2][1].val = '8'
-board[2][2].val = '3'
 
-const displayBoard = board.map(row => row.map(cell => cell.val));
 
 
 
@@ -114,30 +109,69 @@ function checkDiagonal(board) {
         return null
     }
 }
+// This isn't working, need to add in logic to return the right value
+function win(board) {
+    const winningColumn = checkColumn(board);
+    const winningDiag = checkDiagonal(board);
+    const winningRow = checkRow(board);
+
+    if (winningColumn !== null) {
+        console.log(`Winning column with ${winningColumn}`);
+        return winningColumn
+     } else if (winningRow !== null) {
+        console.log(`Winning row with ${winningRow}`);
+        return winningRow
+     } else if (winningDiag !== null) {
+        console.log(`Winning diagonal with ${winningDiag}`);
+        return winningDiag
+     } else {
+        return null
+     }
+}
+
+function playRound(player1, player2){
+    displayBoard()
+    console.log("Player 1's turn");
+    playTurn(player1);
+    console.log("Player 2's turn");
+    playTurn(player2);
+    displayBoard()
+}
+
+function cellPrompt(currentPlayer) {
+    let userRow = prompt(`${currentPlayer.name} enter row number 0, 1, 2:`);
+    let userCol = prompt(`${currentPlayer.name} enter column number 0, 1, 2:`);
+    return [Number(userRow), Number(userCol)];
+}
 
 
+function playTurn(currentPlayer) {
+    let [userRow, userCol] = cellPrompt(currentPlayer);
+    console.log(`${currentPlayer.name} selected cell: ${userRow}, ${userCol}`);
+
+    while (board[userRow][userCol].val !== null) {
+        alert(`${currentPlayer.name}, that cell is already occupied. Try again.`);
+        console.log('Cell is already occupied. Try again.');
+        [userRow, userCol] = cellPrompt(currentPlayer);
+    } 
+    board[userRow][userCol].val = currentPlayer.XorO;
+
+}
 
 
+// Need to turn this into a loop or something
+function playGame() {
+    const player1 = createPlayer('Dave', 'X');
+    const player2 = createPlayer('Bob', 'O');
 
-//game round number variable
+    while (win(board) === null) {
+        playRound(player1, player2);
+    }
 
-//game number variable
-
-//player one goes first. Player 1 turn indicator = true 
-//Player one picks a cell on the grid. Cell value is changed to 'X'. 
-//Change the player 1 turn indicator to false
-
-//player two picks a cell. Cell value is changed to 'O'. 
-//Store values as Round One.
-//After each round, check for winning pattern in a row, column, or the two diagonals. 
-    //If winning pattern detected, check whether 'X' (Player one) or 'O'  (Player two) pattern is detected. Set winner
-//Player one turn indicator is set to true
-
-
-
-
-
-
-
-// each cell should be a p element that can be selected or modified
+    if (win(board) === player1.XorO) {
+        alert(`${player1.name} wins!`) 
+    } else {
+        alert(`${player2.name} wins!`)
+    }
+}
 
